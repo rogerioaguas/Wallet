d9d0cff1de39a829725e199a41626e2e5172b085
Bruno Colombini <bruno.colombini@me.com>
2018-04-14 13:40:23 -0300

    adjusts some lint

--- app/src/main/java/com/example/brunocolombini/wallet/MainActivity.kt
+++ app/src/main/java/com/example/brunocolombini/wallet/MainActivity.kt
@@ -58,22 +58,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
+            R.id.extract -> {
                 // Handle the camera action
             }
-            R.id.nav_gallery -> {
-
-            }
-            R.id.nav_slideshow -> {
-
-            }
-            R.id.nav_manage -> {
-
-            }
-            R.id.nav_share -> {
-
-            }
-            R.id.nav_send -> {
+            R.id.log_out -> {
 
             }
         }
