a54add686622567810588acb1c66f86ae3f57891
Bruno Colombini <bruno.colombini@m4u.com.br>
2018-04-26 17:41:01 -0300

    [LOGINPRESENTER - TEST]

--- app/src/test/java/com/example/brunocolombini/wallet/feature/home/HomePresenterTest.kt
+++ app/src/test/java/com/example/brunocolombini/wallet/feature/home/HomePresenterTest.kt
@@ -3,10 +3,13 @@ package com.example.brunocolombini.wallet.feature.home
 import com.example.brunocolombini.wallet.BaseTest
 import com.example.brunocolombini.wallet.DAO.AppDatabase
 import com.example.brunocolombini.wallet.DAO.infra.UserPreference
+import com.example.brunocolombini.wallet.DAO.user.Extract
+import com.example.brunocolombini.wallet.DAO.user.ExtractDao
 import com.example.brunocolombini.wallet.util.delivery.UpdateBalanceEvent
 import com.example.brunocolombini.wallet.util.enums.BalanceEventType
 import com.nhaarman.mockito_kotlin.eq
 import io.reactivex.Observable
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.subjects.PublishSubject
 import org.junit.Test
@@ -28,10 +31,13 @@ class HomePresenterTest : BaseTest(){
     lateinit var view: HomeContract.View
 
     @Mock
-    lateinit var appDataBase: AppDatabase
+    lateinit var appDatabase: AppDatabase
 
     @Mock
     lateinit var userPreference: UserPreference
+
+    @Mock
+    lateinit var extraDao: ExtractDao
 
     @Test
     fun on_balance_is_updated_success() {
@@ -40,6 +46,21 @@ class HomePresenterTest : BaseTest(){
                 .observeOn(AndroidSchedulers.mainThread())).thenReturn(Observable.just(updateBalance))
         presenter.onAttachView()
         verify(view, times(1)).updateBalance(eq(BalanceEventType.FIAT), eq(10.0))
+
+    }
+
+    @Test
+    fun on_update_user_information_success(){
+        val allCoins: ArrayList<Extract> = ArrayList()
+        allCoins.add(Extract(null, userId, 10.0, "FIAT"))
+        allCoins.add(Extract(null, userId, 10.0, "BITCOIN"))
+        allCoins.add(Extract(null, userId, 10.0, "BRITAS"))
+
+        `when`(userPreference.getUserId()).thenReturn(1)
+        `when`(appDatabase.extractDao()).thenReturn(extraDao)
+        `when`(appDatabase.extractDao().getGroupExtractById(userPreference.getUserId())).thenReturn(Single.just(allCoins))
+        presenter.updateUserInformation()
+        verify(view).updateUserInformation(eq(allCoins))
 
     }
 
